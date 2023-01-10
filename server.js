7384896facc5ae365cc2c0ac2f269fa8e9bb8798
Kyle Jones <KyleJonesWinsted@gmail.com>
2023-01-10 10:11:03 -0600

    Added access token request

--- server.js
+++ server.js
@@ -3,37 +3,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-var CLIENT_ID = process.env.NS_CLIENT;
-var CLIENT_SECRET = process.env.NS_SECRET;
-var SCOPE = process.env.NS_SCOPE;
-var ACCOUNT = process.env.NS_ACCOUNT;
-var REDIRECT_URI = 'https://kylejon.es/netsuite-oauth/'; // Your URL here
-app.use(function (req) {
-    var _a;
+const express_1 = __importDefault(require("express"));
+const https_1 = __importDefault(require("https"));
+const url_1 = require("url");
+const app = (0, express_1.default)();
+const CLIENT_ID = process.env.NS_CLIENT;
+const CLIENT_SECRET = process.env.NS_SECRET;
+const SCOPE = process.env.NS_SCOPE;
+const ACCOUNT = process.env.NS_ACCOUNT;
+const REDIRECT_URI = 'https://kylejon.es/netsuite-oauth/'; // Your URL here
+app.use((req) => {
     console.log('request from', req.url);
-    (_a = req.next) === null || _a === void 0 ? void 0 : _a.call(req);
+    req.next?.();
 });
-app.get('/', function (req, res) {
-    console.log(req.params);
-    var authCode = req.query.code;
-    var refreshToken = req.query.refresh;
+app.get('/', (req, res) => {
+    const authCode = req.query.code;
+    const refreshToken = req.query.refresh;
     if (authCode) {
-        return handleTokenRequest(authCode, 'code', res);
+        return handleTokenRequest(authCode, 'authorization_code', res);
     }
     if (refreshToken) {
         return handleTokenRequest(refreshToken, 'refresh_token', res);
     }
-    var authCodeUrl = createAuthCodeUrl();
+    const authCodeUrl = createAuthCodeUrl();
     res.redirect(authCodeUrl);
 });
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('listening on port 3000');
 });
 function createAuthCodeUrl() {
-    var baseUrl = "https://".concat(ACCOUNT, ".app.netsuite.com/app/login/oauth2/authorize.nl");
-    var params = new URLSearchParams({
+    const baseUrl = `https://${ACCOUNT}.app.netsuite.com/app/login/oauth2/authorize.nl`;
+    const params = new url_1.URLSearchParams({
         response_type: 'code',
         client_id: CLIENT_ID,
         redirect_uri: REDIRECT_URI,
@@ -42,13 +42,62 @@ function createAuthCodeUrl() {
     });
     return baseUrl + '?' + params.toString();
 }
-function handleTokenRequest(grant, grantType, res) {
-    res.send(grantType + ' ' + grant);
+async function handleTokenRequest(grant, grantType, res) {
+    try {
+        const response = await fetchAccessToken(grant, grantType);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(response));
+    }
+    catch (err) {
+        res.send(JSON.stringify(err));
+    }
+}
+async function fetchAccessToken(grant, grantType) {
+    const body = new url_1.URLSearchParams({
+        code: grant,
+        grant_type: grantType,
+        redirect_uri: REDIRECT_URI,
+    }).toString();
+    const headers = {
+        Host: `${ACCOUNT}.suitetalk.api.netsuite.com`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': body.length,
+        Authorization: createBasicAuthString(),
+    };
+    const response = await postRequest(body, headers);
+    if (response.status > 299)
+        throw new Error(`Error fetching token: ${response.status} ${response.statusText}`);
+    return JSON.parse(response.data);
+}
+function postRequest(body, headers) {
+    let data = '';
+    return new Promise((resolve, reject) => {
+        const req = https_1.default.request({
+            hostname: `${ACCOUNT}.suitetalk.api.netsuite.com`,
+            path: '/services/rest/auth/oauth2/v1/token',
+            method: 'POST',
+            headers,
+        }, (res) => {
+            res.on('data', (d) => data += d);
+            res.on('end', () => resolve({
+                data,
+                status: res.statusCode ?? 99,
+                statusText: res.statusMessage ?? 'No status'
+            }));
+            res.on('error', (e) => reject(e));
+        });
+        req.write(body);
+        req.end();
+    });
+}
+function createBasicAuthString() {
+    const authStr = `${CLIENT_ID}:${CLIENT_SECRET}`;
+    return `Basic ${Buffer.from(authStr).toString('base64')}`;
 }
 function generateUUID() {
-    var d = new Date().getTime();
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16;
+    let d = new Date().getTime();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        let r = Math.random() * 16;
         r = (d + r) % 16 | 0;
         d = Math.floor(d / 16);
         return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import fetch from 'node-fetch';
+import https from 'https';
+import { URLSearchParams } from 'url';
 
 const app = express();
 
@@ -15,11 +16,10 @@ app.use((req) => {
 });
 
 app.get('/', (req, res) => {
-    console.log(req.params);
     const authCode = req.query.code as string;
     const refreshToken = req.query.refresh as string;
     if (authCode) {
-        return handleTokenRequest(authCode, 'code', res);
+        return handleTokenRequest(authCode, 'authorization_code', res);
     }
     if (refreshToken) {
         return handleTokenRequest(refreshToken, 'refresh_token', res);
@@ -44,8 +44,58 @@ function createAuthCodeUrl(): string {
     return baseUrl + '?' + params.toString();
 }
 
-function handleTokenRequest(grant: string, grantType: 'code' | 'refresh_token', res: express.Response): void {
-    res.send(grantType + ' ' + grant);
+async function handleTokenRequest(grant: string, grantType: GrantType, res: express.Response): Promise<void> {
+    try {
+        const response = await fetchAccessToken(grant, grantType);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(response));
+    } catch (err) {
+        res.send(JSON.stringify(err));
+    }
+}
+
+async function fetchAccessToken(grant: string, grantType: GrantType): Promise<TokenResponse> {
+    const body = new URLSearchParams({
+        code: grant,
+        grant_type: grantType,
+        redirect_uri: REDIRECT_URI,
+    }).toString();
+    const headers = {
+        Host: `${ACCOUNT}.suitetalk.api.netsuite.com`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': body.length,
+        Authorization: createBasicAuthString(),
+    };
+    const response = await postRequest(body, headers);
+    if (response.status > 299) throw new Error(`Error fetching token: ${response.status} ${response.statusText}`);
+    return JSON.parse(response.data) as TokenResponse;
+}
+
+function postRequest(body: string, headers: Record<string, any>): Promise<PostResponse> {
+    let data = '';
+    return new Promise((resolve, reject) => {
+        const req = https.request({
+            hostname: `${ACCOUNT}.suitetalk.api.netsuite.com`,
+            path: '/services/rest/auth/oauth2/v1/token',
+            method: 'POST',
+            headers,
+        }, (res) => {
+            res.on('data', (d) => data += d);
+            res.on('end', () => resolve({
+                data,
+                status: res.statusCode ?? 99,
+                statusText: res.statusMessage ?? 'No status'
+            }));
+            res.on('error', (e) => reject(e));
+        });
+        req.write(body);
+        req.end();
+    });
+}
+
+function createBasicAuthString(): string {
+    const authStr = `${CLIENT_ID}:${CLIENT_SECRET}`;
+    return `Basic ${Buffer.from(authStr).toString('base64')}`;
 }
 
 function generateUUID(): string {
@@ -58,6 +108,18 @@ function generateUUID(): string {
     });
 }
 
-type Params = {
-    code?: string;
+type TokenResponse = {
+    access_token: string;
+    refresh_token: string;
+    expires_in: 3600;
+    token_type: 'bearer';
+    id_token: unknown;
 }
+
+type PostResponse = {
+    status: number;
+    statusText: string;
+    data: string;
+};
+
+type GrantType = 'authorization_code' | 'refresh_token';
